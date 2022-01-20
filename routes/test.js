e66417136be640133311a17d4e5318a873cba007
woo94 <bestman21c@naver.com>
2022-01-20 15:09:16 +0900

    add test api apidoc

--- routes/test.js
+++ routes/test.js
@@ -3,11 +3,30 @@
 const express = require("express")
 const router = express.Router()
 
-
+/**
+ * @api {get} /test1 Test endpoint1
+ * @apiGroup Test
+ * @apiVersion 0.1.0
+ * @apiSuccessExample {String} Success-Response:
+ *  OK
+ * @apiExample {curl} cURL example
+ * curl -i http://localhost:8080/test1
+ */
 router.get('/test1', (req, res) => {
     res.send("test1 OK")
 })
 
+/**
+ * @api {get} /test2 Test endpoint2
+ * @apiGroup Test
+ * @apiVersion 0.1.0
+ * @apiExample {curl} cURL example
+ * curl -i http://localhost:8080/test2
+ * @apiSuccessExample {json} Success-Response:
+ *  {
+ *      "test": "OK"
+ *  }
+ */
 router.get('/test2', (req, res) => {
     res.json({
         test: "OK"
