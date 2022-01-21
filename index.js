35b9a65b1328b02165cc7a7c045950c0c9fb47d9
woo94 <bestman21c@naver.com>
2022-01-21 15:50:46 +0900

    1) add cookie-parser middleware 2) create custom cors middleware and its error handler

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 const express = require("express")
+const cookieParser = require('cookie-parser')
 const app = express()
 const testRouter = require('./routes/test')
 const userRouter = require('./routes/user')
 
 app.use(express.json())
+app.use(cookieParser())
+
+app.use((req, res, next) => {
+    // handle origin
+    const allowedOrigins = ['http://localhost:3000']
+    if (!allowedOrigins.includes(req.headers.origin)) {
+        throw new Error('not allowed origin')
+    }
+
+    // handle preflight request
+    if (req.method === 'OPTIONS') {
+        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+        res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS,DELETE')
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+        res.setHeader('Access-Control-Allow-Credentials', 'true')
+        res.setHeader('Content-Length', '0')
+        res.sendStatus(204)
+    }
+    else {
+        next()
+    }
+})
+
+// error handler
+app.use((err, req, res, next) => {
+    res.status(400).send(err.message)
+})
+
 app.use('/', testRouter)
 app.use('/user', userRouter)
 
